5b3da2b7fc043172a5a22aa4bd53fde33b41bc9b
jimmyale3102 <jimmyale3102@gmail.com>
2024-05-20 14:24:25 +1000

    Change home colors and components

--- app/src/main/java/dev/alejo/triqui/ui/theme/Color.kt
+++ app/src/main/java/dev/alejo/triqui/ui/theme/Color.kt
@@ -10,6 +10,7 @@ val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
+val DarkBackground = Color(0xFF1A1A1A)
 val DarkPurple = Color(0xFF363062)
 val LightBlueGray = Color(0xFF818FB4)
 val CreamyWhite = Color(0xFFF5E8C7)
